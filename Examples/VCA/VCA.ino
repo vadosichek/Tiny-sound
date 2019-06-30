ca8fb806e2999377b6b894372b69274b0fc4d455
vadosichek <big-vadim@mail.ru>
2019-06-30 18:05:30 +0300

    examples

---
+++ Examples/VCA/VCA.ino
@@ -0,0 +1,23 @@
+#include <TinySound.h>
+#include <Vca.h>
+
+TinySound ts;
+Vca vca;
+
+void setup() {
+  vca.init();
+  ts.init();
+}
+
+void loop() {}
+
+ISR(ADC_vect){ // interupt triggered ad completion of ADC counter
+  ts.update();
+  if(ts.analogRelevant(2)) vca.setAmplification(ts.analogValues[2]);
+}
+
+ISR(TIMER1_COMPA_vect){  //audiorate interrupt
+  int value = vca.render(ts.analogValues[3], 0);
+  OCR0A = value;
+  OCR0B = value;
+}
---
+++ Examples/VCO/VCO.ino
@@ -0,0 +1,30 @@
+#include <TinySound.h>
+#include <Vco.h>
+
+TinySound ts;
+Vco osc;
+
+void setup() {
+  osc.init();
+  ts.init();
+  osc.setMode(2);
+  osc.setXYmode(true);
+  osc.setFrequency(0);
+  osc.setFrequency2(64);
+  osc.setFrequency3(64);
+}
+
+void loop() {
+  osc.update();
+}
+
+ISR(ADC_vect){ // interupt triggered ad completion of ADC counter
+  ts.update();
+  if(ts.analogRelevant(2)) osc.setFrequency(ts.analogValues[2]);
+}
+
+ISR(TIMER1_COMPA_vect){  //audiorate interrupt
+  OCR0A = osc.sample;
+  OCR0B = osc.sample2;
+  osc.render();
+}
